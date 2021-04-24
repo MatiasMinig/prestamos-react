4910daddaf802e5a00584883f07e2faf4ccf5d89
matiasminig <matiasminig@gmail.com>
2021-04-24 19:21:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment,useState} from 'react';
+import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
+import Mensaje from './componentes/Mensaje';
+import Resultado from './componentes/Resultado';
 
 function App() {
+    // Definir el State  
+    const [cantidad, guardarCantidad] = useState(0);
+    const [plazo, guardarPlazo] = useState('');
+    const [total, guardarTotal] = useState(0);
+
+    // comprobacion del mensaje
+    let componente;
+    if(total === 0){
+      componente = <Mensaje/>
+    }else {
+      componente =  <Resultado
+                        total={total}
+                        plazo={plazo}
+                        cantidad={cantidad}
+                    />
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+        <Header
+            titulo="Cotizador de Prestamos"
+        />
+        <div className="container">
+            <Formulario
+              cantidad={cantidad}
+              guardarCantidad={guardarCantidad}
+              plazo={plazo}
+              guardarPlazo={guardarPlazo}
+              total={total}
+              guardarTotal={guardarTotal}
+            />
+          <div className='mensajes'>
+            {componente}
+          </div>
+        </div>
+     </Fragment>
   );
 }
 
